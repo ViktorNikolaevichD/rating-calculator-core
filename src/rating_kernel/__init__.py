eefc47f4396a4cd3afd6ef48d806ca4f0a2dac7d
Victor <10xl.pro@mail.ru>
2026-04-24 12:28:10 +0300

    Добавить публичный апи для импорта пакетов напрямую

--- src/rating_kernel/__init__.py
+++ src/rating_kernel/__init__.py
@@ -1,5 +1,11 @@
 from .domain.value_objects.reviewer import Reviewer
-from .domain.value_objects.feedback import Feedback
+from .domain.value_objects.feedback import Feedback, FeedbackMarkWithWeight
 from .domain.exceptions import DomainError, ZeroTotalMarkWeightError
 
-__all__ = ["Reviewer", "Feedback", "DomainError", "ZeroTotalMarkWeightError"]
+__all__ = [
+    "Reviewer",
+    "Feedback",
+    "FeedbackMarkWithWeight",
+    "DomainError",
+    "ZeroTotalMarkWeightError",
+]
--- src/rating_weighted/__init__.py
+++ src/rating_weighted/__init__.py
@@ -1,5 +1,16 @@
 from .domain.value_objects.configuration import WeightedRatingConfiguration
+from .domain.value_objects.rating_context import RatingContext, RatingContextWithMarkWeights
+from .domain.services.final_rating_calculator import FinalRatingCalculator
+from .domain.services.mark_weight_calculator import MarkWeightCalculator
+from .domain.services.penalty_calculator import PenaltyCalculator
+from .domain.services.trust_factor_calculator import TrustFactorCalculator
 
 __all__ = [
     "WeightedRatingConfiguration",
+    "RatingContext",
+    "RatingContextWithMarkWeights",
+    "TrustFactorCalculator",
+    "PenaltyCalculator",
+    "MarkWeightCalculator",
+    "FinalRatingCalculator",
 ]
