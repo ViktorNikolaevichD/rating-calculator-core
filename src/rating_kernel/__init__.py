1139929da2dc3ffaad10401d00a1e29d32392309
Victor <10xl.pro@mail.ru>
2026-04-24 12:52:52 +0300

    Расширить публичные экспорты полностью

--- src/rating_kernel/__init__.py
+++ src/rating_kernel/__init__.py
@@ -1,11 +1,34 @@
-from .domain.value_objects.reviewer import Reviewer
-from .domain.value_objects.feedback import Feedback, FeedbackMarkWithWeight
+from .domain.value_objects.reviewer import (
+    Reviewer,
+    ReviewerActivityProfile,
+    ReviewerIdentity,
+    ReviewerReputationHistory,
+)
+from .domain.value_objects.feedback import (
+    Feedback,
+    FeedbackContent,
+    FeedbackContentWithMarkWeights,
+    FeedbackMark,
+    FeedbackMarkWithWeight,
+    FeedbackModerationState,
+    FeedbackTimingContext,
+    FeedbackWithMarkWeights,
+)
 from .domain.exceptions import DomainError, ZeroTotalMarkWeightError
 
 __all__ = [
     "Reviewer",
+    "ReviewerIdentity",
+    "ReviewerActivityProfile",
+    "ReviewerReputationHistory",
     "Feedback",
+    "FeedbackMark",
+    "FeedbackContent",
+    "FeedbackModerationState",
+    "FeedbackTimingContext",
     "FeedbackMarkWithWeight",
+    "FeedbackContentWithMarkWeights",
+    "FeedbackWithMarkWeights",
     "DomainError",
     "ZeroTotalMarkWeightError",
 ]
--- src/rating_weighted/__init__.py
+++ src/rating_weighted/__init__.py
@@ -1,4 +1,16 @@
-from .domain.value_objects.configuration import WeightedRatingBaseConfig, WeightedRatingConfiguration
+from .domain.value_objects.configuration import (
+    AggregatedPenaltyConfig,
+    BehaviorPenaltyConfig,
+    FeedbackWeightAggregationConfig,
+    MarkPenaltyConfig,
+    RangeOfFinalMarkWeight,
+    RatingFactorsConfig,
+    TemporalConfig,
+    TextLengthThresholds,
+    TextPenaltyConfig,
+    WeightedRatingBaseConfig,
+    WeightedRatingConfiguration,
+)
 from .domain.value_objects.rating_context import RatingContext, RatingContextWithMarkWeights
 from .domain.services.final_rating_calculator import FinalRatingCalculator
 from .domain.services.mark_weight_calculator import MarkWeightCalculator
@@ -8,6 +20,15 @@ from .domain.services.trust_factor_calculator import TrustFactorCalculator
 __all__ = [
     "WeightedRatingConfiguration",
     "WeightedRatingBaseConfig",
+    "RatingFactorsConfig",
+    "TextLengthThresholds",
+    "TextPenaltyConfig",
+    "MarkPenaltyConfig",
+    "BehaviorPenaltyConfig",
+    "TemporalConfig",
+    "AggregatedPenaltyConfig",
+    "RangeOfFinalMarkWeight",
+    "FeedbackWeightAggregationConfig",
     "RatingContext",
     "RatingContextWithMarkWeights",
     "TrustFactorCalculator",
